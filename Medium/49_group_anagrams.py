a43dc14c94584b4a473bae95ee500d4454bd7c1b
sudormsociallife <gmchong060615@gmail.com>
2025-08-06 23:49:04 +0800

    added solution for 49. Group Anagrams

---
+++ Medium/49_group_anagrams.py
@@ -0,0 +1,26 @@
+# What are the conditions of an anagram? The conditions are two words must have the same length, each letter in the word is used exactly once.
+# But what that tells us is that if words are sorted a..z, and the result are equal, they are also anagrams
+# We can sort words and group the same sorted words together, effectively grouping anagrams
+
+# Question: Why use tuple as the key for Hash Map?
+# Answer: Dictionary requires keys to be IMMUTABLE and HASHABLE, and tuple is the perfect data structure for that since it is IMMUTABLE and HASHABLE unlike list, set and dict. 
+# Lists, sets, and dicts are mutable (can change), so Python doesn't allow them as keys since their hash could change.
+
+# Question: Why does tuple(sorted(i)) works?
+# Answer: sorted(i), when iterating through a for loop returns a list of characters, but lists are not hashable and not immutable, so Python does not allow them as dictionary keys. But tuple freezes the list into a IMMUTABLE and HASHABLE object, so we can use tuple as a key to group matching words
+
+# Bonus: Using an empty string also works since strings are also IMMUTABLE and HASHABLE.
+# key = "".join(sorted(i)) also works
+
+class Solution(object):
+    def groupAnagrams(self, strs):
+        hashMap = {}
+
+        for i in strs: #iterate each word in the list
+            key = tuple(sorted(i)) #initialize the key as the sorted word for each word
+            if key in hashMap:
+                hashMap[key].append(i) #append the word as value to the same sorted word that already exists in the hashmap
+            else:
+                hashMap[key] = [i] #append a new word as the value
+
+        return list(hashMap.values()) #since the values in the hashmap is already a list of anagrams (grouped), simply return the hashmap in the form of a list
