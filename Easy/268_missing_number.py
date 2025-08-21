0f9acaa1a2baa127605004491be4f6a8dd13e1e0
sudormsociallife <194980529+sudo-sanoo@users.noreply.github.com>
2025-08-21 12:41:05 +0800

    Update 268_missing_number.py

--- Easy/268_missing_number.py
+++ Easy/268_missing_number.py
@@ -4,7 +4,12 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
+        # Improved Solution
+        n = len(nums) # ranges of number supposed to be in the list
+        total = n * (n + 1) // 2 # supposed sum if all the list of numbers are complete with no missing number
+        return total - sum(nums) # supposed sum - current sum will get the answer
 
+        '''
         for i in nums:
             if i+1 not in nums and i+1 == len(nums):
                 return i+1
@@ -14,3 +19,4 @@ class Solution(object):
                 continue
 
         return -1
+        '''
