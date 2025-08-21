490fbe02ddf6d6e669199a82b5f42e3ce3df8b51
sudormsociallife <194980529+sudo-sanoo@users.noreply.github.com>
2025-08-22 00:20:21 +0800

    improved solution for 26. Remove Duplicates from Sorted Array

--- Easy/26_remove_duplicates_from_sorted_array.py
+++ Easy/26_remove_duplicates_from_sorted_array.py
@@ -6,8 +6,21 @@ class Solution(object):
         """
         # An in-place algorithm modifies the array directly without creating another full copy.
         # It may still use a small, constant amount of extra memory (e.g., variables or pointers).
-        # Improved Approach:
-        # (solution in progress)
+        # Improved Approach: Using "k" as a comparative that keeps being updated, and "i" to traverse the list
+        #    - No need for removing elements or keeping track of the length of the list
+        #    - If compared is not equal, then it should be in the list, if not, then it does not belong there
+        #    - "k" is used as a count of how many elements should be in the list
+        #    - Runtime Complexity: O(n)
+        if len(nums) == 0:
+            return 0
+
+        k = 1
+        for i in range(1, len(nums)):
+            if nums[i] != nums[k-1]:
+                nums[k] = nums[i]
+                k += 1
+
+        return k
         
         # Approach 1: Brute-force, uneccessary .remove() and while loop worse case having to loop n times.
         #    - Runtime Complexity of O(n**2)
