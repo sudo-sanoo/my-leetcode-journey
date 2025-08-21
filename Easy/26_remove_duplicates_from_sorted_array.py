80600b3bb284f4d9c0e2abd90c34a2eb7c33ed5c
sudormsociallife <194980529+sudo-sanoo@users.noreply.github.com>
2025-08-21 23:55:11 +0800

    Update 26_remove_duplicates_from_sorted_array.py

--- Easy/26_remove_duplicates_from_sorted_array.py
+++ Easy/26_remove_duplicates_from_sorted_array.py
@@ -6,6 +6,11 @@ class Solution(object):
         """
         # An in-place algorithm modifies the array directly without creating another full copy.
         # It may still use a small, constant amount of extra memory (e.g., variables or pointers).
+        # Improved Approach:
+        # (solution in progress)
+        
+        # Approach 1: Brute-force, uneccessary .remove() and while loop worse case having to loop n times.
+        #    - Runtime Complexity of O(n**2)
         result = len(nums)
         base = 0
         compare = 1
