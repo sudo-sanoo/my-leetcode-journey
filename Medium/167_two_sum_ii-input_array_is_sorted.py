b71bc002a50a8f15ba28228cdc52487bf167c485
sudormsociallife <194980529+sudo-sanoo@users.noreply.github.com>
2025-09-04 04:57:42 +0800

    Update 167_two_sum_ii-input_array_is_sorted.py

--- Medium/167_two_sum_ii-input_array_is_sorted.py
+++ Medium/167_two_sum_ii-input_array_is_sorted.py
@@ -5,6 +5,22 @@ class Solution(object):
         :type target: int
         :rtype: List[int]
         """
+        # Approach 2: Two Pointers
+        i = 0
+        j = len(numbers)-1
+        while i < j:
+            if numbers[i] + numbers[j] < target:
+                i += 1
+                continue
+            elif numbers[i] + numbers[j] > target:
+                j -= 1
+            else:
+                return [i+1, j+1]
+
+        return -1
+
+        '''
+        # Approach 1: HashMap
         numMap = {}
 
         for index, value in enumerate(numbers):
@@ -14,3 +30,4 @@ class Solution(object):
                 return [numMap[target-value]+1, index+1]
 
         return -1
+        '''
