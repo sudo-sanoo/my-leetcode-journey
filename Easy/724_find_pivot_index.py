e4acc07b2cfee280a3a766e006be4c933403ba9b
sudormsociallife <194980529+sudo-sanoo@users.noreply.github.com>
2025-10-14 18:22:20 +0800

    added solution for 724. Find Pivot Index

--- Easy/724_find_pivot_index.py
+++ Easy/724_find_pivot_index.py
@@ -6,20 +6,22 @@ class Solution(object):
         """
         # 3 indices, i, j and k where k is the pivot index
         # return the pivot index, where sum(i to k-1) == sum(k+1 to j) else -1
-        # sum(i to k-1) = ps[k-1] - ps[i]
-        # sum(k+1 to j) = ps[j] - ps[k+1]
+        # sum(i to k-1) can be represented as "left"
+        # sum(k+1 to j) can be represented as "right"
+        # left = ps[k-1] - ps[i]
+        # right = ps[j] - ps[k+1]
 
         # Optimized approach
         left, right = 0, 0
 
-        for i in nums:
-            right += i
+        for j in nums:
+            right += j
 
-        for j in range(len(nums)):
-            left += nums[j-1] if j - 1 >= 0 else 0
-            right -= nums[j]
+        for i in range(len(nums)):
+            left += nums[i-1] if i - 1 >= 0 else 0
+            right -= nums[i]
             if left == right:
-                return j
+                return i
 
         return -1
 
