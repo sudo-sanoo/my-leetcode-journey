25f3a7f04211937df1514fad4f51ff3fc34fe8e9
sudormsociallife <194980529+sudo-sanoo@users.noreply.github.com>
2025-11-04 02:20:22 +0800

    added solution for 1578. Minimum Time to Make Rope Colorful
    Implemented an optimized solution for calculating the minimum cost to make the rope colorful by using a single pass through the colors and adjusting needed times accordingly.

---
+++ Medium/1578_minimum_time_to_make_rope_colorful.py
@@ -0,0 +1,41 @@
+class Solution(object):
+    def minCost(self, colors, neededTime):
+        """
+        :type colors: str
+        :type neededTime: List[int]
+        :rtype: int
+        """
+        # Optimized Solution
+        res = 0
+        for i in range(1, len(colors)):
+            if colors[i] == colors[i-1]:
+                res += min(neededTime[i], neededTime[i-1])
+                # Keep the larger neededTime (cuz we already used the minimum, added to res)
+                # Update the current neededTime for the next min() comparison
+                neededTime[i] = max(neededTime[i], neededTime[i-1])
+        return res
+
+        # Initial Solution (Idea)
+        res = 0
+        prevColor = colors[0]
+        prevTime = neededTime[0]
+        time = []
+        for i, c, t in zip(range(len(colors)), colors, neededTime):
+            if c == prevColor:
+                if i == 0:
+                    continue
+                if not time:
+                    time.append(prevTime)
+                time.append(t)
+            else:
+                if time:
+                    res += (sum(time) - max(time))
+                    time = []
+
+            prevColor = c
+            prevTime = t
+
+        if time:
+            res += (sum(time) - max(time))
+
+        return res
