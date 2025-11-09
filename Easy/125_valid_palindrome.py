6beeab28b65a01c593a4a93948e0785c8c9e7e92
sudormsociallife <194980529+sudo-sanoo@users.noreply.github.com>
2025-11-09 19:28:36 +0800

    Update 125_valid_palindrome.py, added two pointers solution

--- Easy/125_valid_palindrome.py
+++ Easy/125_valid_palindrome.py
@@ -1,3 +1,23 @@
+# Using Two Pointers
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        if len(s) == 1:
+            return True
+
+        s = [c.lower() for c in s if c.isalnum()]
+        l = 0
+        r = len(s)-1
+
+        while l < r:
+            if s[l] == s[r]:
+                l+=1
+                r-=1
+            else:
+                return False
+
+        return True
+        
+# Using Regex solution
 import re
 
 class Solution(object):
