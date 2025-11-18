83e99df9a3e8ed0f4e608560269a24fe70b1e8fe
sudormsociallife <194980529+sudo-sanoo@users.noreply.github.com>
2025-11-18 10:01:02 +0800

    added solution for 717. 1-bit and 2-bit Characters

---
+++ Easy/717_1-bit_and_2-bit_characters.py
@@ -0,0 +1,14 @@
+class Solution:
+    def isOneBitCharacter(self, bits: List[int]) -> bool:
+        n = len(bits)
+        i = 0
+        while i < n-1:
+            if bits[i] == 1:
+                i += 2
+            else:
+                i += 1
+
+        if i == n:
+            return False
+        
+        return True
