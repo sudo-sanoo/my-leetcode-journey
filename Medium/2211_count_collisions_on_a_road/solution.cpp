7a355924d4df146da3c5cbb13c0ee5693998d97c
sudormsociallife <194980529+sudo-sanoo@users.noreply.github.com>
2025-12-07 13:32:08 +0800

    added solution for 2211. Count Collisions on a Road

---
+++ Medium/2211_count_collisions_on_a_road/solution.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int countCollisions(string directions) {
+        int n = directions.size();
+        int l = 0, r = n - 1;
+        while (l < n && directions[l] == 'L') {
+            l++;
+        }
+        while (r >= l && directions[r] == 'R') {
+            r--;
+        }
+        int collision = 0;
+        for (int i = l; i <= r; i++) {
+            if (directions[i] != 'S') {
+                collision++;
+            }
+        }
+        return collision;
+    }
+};
